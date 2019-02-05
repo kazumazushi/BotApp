11c8b5e92af7f39fccf6a98e60bcf98d50ce41da
Cloud User <ec2-user@test-host-zushi.com>
2019-02-05 09:12:02 +0000

    process of db_auth

--- db_session.py
+++ db_session.py
@@ -1,20 +1,68 @@
 #! /usr/bin/python
 
 import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import declarative_base as dec
+#from setting import Base
+#from setting import ENGINE
+import urllib as ul
 
-url = sa.create_engine("mysql+pymysql://root:root@localhost/auth_info?charset=utf8", echo=True, encoding='utf-8', max_overflow=10, pool_size=6)
-url.connect()
-print(url)
+##session
 
-#engine = sa.create_engine(url, echo=True)
+class DB_state:
+	def __init__(self):
+		self.engine = sa.create_engine("mysql+pymysql://root:root@localhost/auth_info?charset=utf8", echo=False, encoding='utf8-', max_overflow=10, pool_size=6)
+		self.engine.connect()
 
-#engine.execute('DROP TABLE zoo')
-#engine.execute('CREATE TABLE zoo (critter VARCHAR(20) PRIMARY KEY, count INT, damages FLOAT)')
+	def db_communication(self):
+		session = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = self.engine))
+		if session == "":
+			print("no session and try it again")
+		else:
+			print("having session and the session is {}".format(session))
 
-# SQL文に「?」が使用できないので、代わりに「%s」を使用
-#ins = "INSERT INTO zoo (critter, count, damages) VALUES (%s, %s, %s)"
-#engine.execute(ins, "あひる", 10, 0.0)
-#engine.execute(ins, "くま", 2, 1000.0)
-#engine.execute(ins, "いたち", 1, 2000.0)
-#rows = engine.execute('SELECT * FROM zoo')
+	def declaration(self):
+		Base = declarative_base():
+		Base.query = session.query_property()
+
+
+
+
+## model
+
+class Query_table(Base):
+
+	def usertable(self):
+		__tablename__ = 'accepted'
+		id = sa.Column('id', Integer, auto_increment, primary_key = True)
+		self.db_account = sa.Column('account', Varchar(255))
+		self.db_passwoed = sa.Column('password', Varchar(255))
+		self.db_email = sa.Column('email', Varchar(255))
+
+"""
+	def query_auth(self,,):
+		result = self.session.query(Test).filter(Test.id == id).all()
+		for row in result:
+			print(row.id, row.name)
+"""
+
+	def main(args):
+		Base.metadata.create_all(bind=self.engine)
+
+	def db_query_user(self):
+		pass
+
+	def db_query_password(self):
+		pass
+
+
+
+if __name__=='__main__':
+	conn = DB_state()
+	conn.db_communication()
+	users = session.query(self.db_account).all()
+	for user in users:
+		print(self.db_account)
 
--- login.py
+++ login.py
@@ -3,60 +3,42 @@ import time
 import sys
 import tkinter as tk
 import tkinter.messagebox as tm
-from db_sessiion import *
-from db_model import * 
+import db_session
 
 class Loginwindow(tk.Frame):
-	def __init__(self, windpw):
-		super().__init__(window)
-		self.title("Welcome to Chat")
-		self.geometry('300x200')
-		self.account_label = tk.Label(self, text='Account')
-		self.password_label = tk.Label(self, text='Password')
-		self.account_input = tk.Entry(self)
-		self.password_input = tk.Entry(self)
+	def __init__(self, master):
+		super().__init__(master)
+#		self.title("Welcome to Chat")
+#   	self.geometry('300x200')
+		self.account_label = tk.Label(master, text='Account')
+		self.password_label = tk.Label(master, text='Password')
+		self.account_input = tk.Entry(master)
+		self.password_input = tk.Entry(master)
 		self.account_label.place(x=30 , y=50)
 		self.password_label.place(x=30 , y=70)
 		self.account_input.place(x=90 , y=50)
 		self.password_input.place(x=90 , y=70) 
-		self.login_button =  tk.Button(self, text='Logon', width=10, command=self.destroy)
+		self.login_button =  tk.Button(master, text='Logon', width=10, command=self._login_btn_clicked)
 		self.login_button.place(x=70 , y=100)
-		self.cancel_button =  tk.Button(self text='Cancel', width=10, command=self.destroy)
+		self.cancel_button =  tk.Button(master, text='Cancel', width=10, command=self._cancel_btn_clicked)
 		self.cancel_button.place(x=180 , y=100)
 
-    def _login_btn_clicked(self):
-        # print("Clicked")
-        username = self.entry_username.get()
-        password = self.entry_password.get()
+	def _login_btn_clicked(self):
+		username = self.account_input.get()
+		password = self.password_input.get()
 
-        # print(username, password)
+		if username == "" and password == "":
+			tm.showinfo("Login info", " Welcome")
+		else:
+			tm.showerror("Login error", "Incorrect username")	
 
-        if username == "" and password == "":
-            tm.showinfo("Login info", "Welcome")
-        else:
-            tm.showerror("Login error", "Incorrect username")
+	def _cancel_btn_clicked(self):
+		quit()
 
-
-root = tk.Tk()
-lf = Loginwindow(root)
-root.mainloop()
-
-#window = tk.Tk()
-#window.title("Welcome to Chat")
-#window.geometry('300x200')
-#account_label = tk.Label(window, text='Account')
-#password_label = tk.Label(window, text='Password')
-#account_input = tk.Entry(window)
-#password_input = tk.Entry(window)
-#account_label.place(x=30 , y=50)
-#password_label.place(x=30 , y=70)
-#account_input.place(x=90 , y=50)
-#password_input.place(x=90 , y=70) 
-#login_button =  tk.Button(window, text='Logon', width=10, command=window.destroy)
-#login_button.place(x=70 , y=100)
-#cancel_button =  tk.Button(window, text='Cancel', width=10, command=window.destroy)
-#cancel_button.place(x=180 , y=100)
-#cancel_button = tk.Button(window, text='Logon', width=15, command=window.destroy)
-#cancel_button.pack(padx=15, side=LEFT)
-#window.mainloop()
-
+if __name__=='__main__':
+	root = tk.Tk()
+	root.title("Welcome to Chat")
+	root.geometry('300x200')
+	app = Loginwindow(root)
+	app.mainloop()
+	
